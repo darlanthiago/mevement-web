03c31836f0763fd30b3c9d4d29cdb5ae74efa00d
Darlan Thiago <darlanthiago53@gmail.com>
2022-02-22 16:16:38 -0300

    Add meta assets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Image
-        className={styles.image}
-        src="/logo.svg"
-        alt="mevent"
-        width={1084}
-        height={542}
-      />
-    </div>
+    <>
+      <Head>
+        <title>mevent | Gerencimento inteligente de eventos</title>
+      </Head>
+      <div className={styles.container}>
+        <Image
+          className={styles.image}
+          src="/logo.svg"
+          alt="mevent"
+          width={1084}
+          height={542}
+        />
+      </div>
+    </>
   );
 };
 
